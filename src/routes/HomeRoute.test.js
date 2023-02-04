7dad7d529d5a463d05ed46faef3fb0ecff2fdada
Edward <edward821220@gmail.com>
2023-02-04 16:14:55 +0800

    finished HomeRoute test

--- src/routes/HomeRoute.test.js
+++ src/routes/HomeRoute.test.js
@@ -6,13 +6,12 @@ import HomeRoute from "./HomeRoute";
 
 const handler = [
   rest.get("/api/repositories", (req, res, ctx) => {
-    const query = req.url.searchParams.get("q");
-    console.log(query);
+    const language = req.url.searchParams.get("q").split("language:")[1];
     return res(
       ctx.json({
         items: [
-          { id: 1, full_name: "full name!" },
-          { id: 2, full_name: "full name 2!" },
+          { id: 1, full_name: `${language}_one` },
+          { id: 2, full_name: `${language}_two` },
         ],
       })
     );
@@ -34,4 +33,30 @@ afterEach(() => {
 // 在所有測試之後執行
 afterAll(() => {
   server.close();
+});
+
+test("render two links for each language.", async () => {
+  render(
+    <MemoryRouter>
+      <HomeRoute />
+    </MemoryRouter>
+  );
+  const languages = [
+    "javascript",
+    "typescript",
+    "java",
+    "rust",
+    "go",
+    "python",
+  ];
+  for (let language of languages) {
+    const link = await screen.findAllByRole("link", {
+      name: new RegExp(`${language}_`),
+    });
+    expect(link).toHaveLength(2);
+    expect(link[0]).toHaveTextContent(`${language}_one`);
+    expect(link[1]).toHaveTextContent(`${language}_two`);
+    expect(link[0]).toHaveAttribute("href", `/repositories/${language}_one`);
+    expect(link[1]).toHaveAttribute("href", `/repositories/${language}_two`);
+  }
 });
