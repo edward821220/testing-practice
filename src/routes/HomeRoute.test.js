bac00c92df50b3f0fdea36fb2f7a05f4cb6357f5
Edward <edward821220@gmail.com>
2023-02-04 10:21:27 +0800

    set MSW

--- src/routes/HomeRoute.test.js
+++ src/routes/HomeRoute.test.js
@@ -3,3 +3,35 @@ import { setupServer } from "msw/node";
 import { rest } from "msw";
 import { MemoryRouter } from "react-router";
 import HomeRoute from "./HomeRoute";
+
+const handler = [
+  rest.get("/api/repositories", (req, res, ctx) => {
+    const query = req.url.searchParams.get("q");
+    console.log(query);
+    return res(
+      ctx.json({
+        items: [
+          { id: 1, full_name: "full name!" },
+          { id: 2, full_name: "full name 2!" },
+        ],
+      })
+    );
+  }),
+];
+
+const server = setupServer(...handler);
+
+// 在所有測試之前執行
+beforeAll(() => {
+  server.listen();
+});
+
+// 在每個測試後執行
+afterEach(() => {
+  server.resetHandlers();
+});
+
+// 在所有測試之後執行
+afterAll(() => {
+  server.close();
+});
